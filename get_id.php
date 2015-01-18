e223b475384b50bdedf9293fe32184a6a58b6d56
CPH <hypery11@gmail.com>
2015-01-18 22:45:08 +0800

    main

--- get_id.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo file_get_contents("https://api.instagram.com/v1/users/search?client_id=bf550b87688947d4a04f7567696dafaf&q=".$_GET["id"]);
-
-?>
--- query.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if($_GET["id"])
-file_put_contents("log.txt",$_SERVER["REMOTE_ADDR"]."\t".date("Y-m-d l H:i:s")."\t".$_GET["id"]."\n",FILE_APPEND);
-
-?>
